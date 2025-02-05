edca5e1a94d764987450e9fbaa511d85b4f4aae2
Edward Ojeda <edward.ojeda.es@gmail.com>
2025-02-05 15:42:32 -0500

    Image in login

--- frontend/src/pages/public/Login.jsx
+++ frontend/src/pages/public/Login.jsx
@@ -4,7 +4,8 @@ export function Login() {
   return (
     <div className="flex">
       <div className="bg-blue-800 text-white bg-[url(/logo.png)] w-lg h-screen bg-no-repeat bg-top">
-        <p className="w-auto p-12 font-bold relative top-100">
+        <img className="mx-auto" src="/login-presentation.png" />
+        <p className="w-auto p-12 font-bold relative bottom-20">
           "Conecta, aprende y crece. Bienvenido a EventHub, tu puerta a
           experiencias que inspiran."
         </p>

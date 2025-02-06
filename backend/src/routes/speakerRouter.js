3fc62636b9edc5b2bb1c0bb862998f82f6568fb8
Edward Ojeda <edward.ojeda.es@gmail.com>
2025-02-06 16:05:23 -0500

    add routes to speaker

--- backend/src/routes/speakerRouter.js
+++ backend/src/routes/speakerRouter.js
@@ -1,7 +1,9 @@
 import express from "express";
 import {
   createSpeaker,
+  deleteSpeaker,
   getAllSpeakers,
+  updateSpeaker,
 } from "../controllers/speakerController.js";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
 
@@ -9,5 +11,7 @@ const router = express.Router();
 
 router.post("/createSpeaker", authMiddleware, createSpeaker);
 router.get("/getSpeakers", getAllSpeakers);
+router.put("/update/:id", authMiddleware, updateSpeaker);
+router.delete("/delete/:id", authMiddleware, deleteSpeaker);
 
 export default router;

b6a2263121271e20512ff91916ef3baf475f040d
Edward Ojeda <edward.ojeda.es@gmail.com>
2025-02-28 16:17:12 -0500

    Add Fields EventForm

--- frontend/src/components/EventForm.jsx
+++ frontend/src/components/EventForm.jsx
@@ -1,3 +1,29 @@
 export function EventForm() {
-  return <h1>Hola desde EventForm</h1>;
+  return (
+    <>
+      <form>
+        <legend>Crear Evento</legend>
+        <div>
+          <label>Titulo</label>
+          <input />
+        </div>
+        <div>
+          <label>Descripción</label>
+          <input />
+        </div>
+        <div>
+          <label>Fecha</label>
+          <input />
+        </div>
+        <div>
+          <label>Ubicación</label>
+          <input />
+        </div>
+        <div>
+          <label>Speakers</label>
+          <input />
+        </div>
+      </form>
+    </>
+  );
 }

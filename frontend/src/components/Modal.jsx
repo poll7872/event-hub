d2859d505a8f4dc0e2b732ddc98ee1f42c0fdb12
Edward Ojeda <edward.ojeda.es@gmail.com>
2025-03-01 14:57:04 -0500

    Plugin tailwind scrollbar

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -6,13 +6,23 @@ export function Modal({ isOpen, onClose, children }) {
   return (
     <div className="fixed inset-0 flex items-center justify-center z-50">
       {/* Fondo semitransparente */}
-      <div className="absolute inset-0 bg-black opacity-50 backdrop-blur-sm"></div>
+      <div
+        className="absolute inset-0 bg-black opacity-50 backdrop-blur-sm"
+        onClick={onClose}
+      ></div>
 
-      {/* Contenido del modal con animación */}
-      <div className="bg-white rounded-lg shadow-lg p-6 w-96 relative mx-4 z-10 animate-[var(--animate-fade-in-down)]">
-        <button className="absolute -top-0 right-1" onClick={onClose}>
+      {/* Contenido del modal */}
+      <div className="bg-white rounded-lg shadow-lg p-6 w-96 max-h-[90vh] overflow-y-auto relative mx-4 z-10 animate-[var(--animate-fade-in-down)]">
+        {/* Botón de cerrar */}
+        <button
+          className="absolute top-2 right-2"
+          onClick={onClose}
+          aria-label="Cerrar modal"
+        >
           <DismissCircleFilled className="text-red-500 text-xl hover:text-red-400 cursor-pointer" />
         </button>
+
+        {/* Contenido del modal */}
         {children}
       </div>
     </div>
